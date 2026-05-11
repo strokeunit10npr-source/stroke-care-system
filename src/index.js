2a1725cbf29a628ee972ffb7fdc3eeb363d0b2ec
strokeunit10npr-source <strokeunit10.npr@gmail.com>
2026-05-12 04:41:15 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+// เชื่อมต่อโค้ด React เข้ากับ <div id="root"> ในไฟล์ index.html
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
